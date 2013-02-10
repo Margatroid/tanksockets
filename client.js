88f29b13d07b0c3e19a463b7f32826778ba22b6c
Alvis Duong <alvisduong@gmail.com>
2013-02-10 15:05:10 +0000

    Setup bike object

--- client.js
+++ client.js
@@ -1,4 +1,96 @@
+(function(){
+  var requestAnimationFrame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
 var socket = io.connect('/');
 socket.on('onconnected', function(data) {
   console.log('Received UUID of ' + data.id + ' from socket.io');
+});
+
+var canvas;
+var context;
+var canvasSize = { height: 100, width: 100 }
+
+var bike = {
+  x: 0,
+  y: 0,
+  direction: 'e',
+  color: '#000000'
+};
+
+bike.move = function()
+{
+  switch(this.direction)
+  {
+    case 'n': this.y -= 1; break;
+    case 's': this.y += 1; break;
+    case 'w': this.x -= 1; break;
+    case 'e': this.x += 1; break;
+  }
+};
+
+canvasHelper = {
+  blockSize: 5
+};
+
+canvasHelper.drawBike = function(bike)
+{
+  context.fillRect(
+    bike.x * this.blockSize,
+    bike.y * this.blockSize,
+    this.blockSize,
+    this.blockSize
+  );
+};
+
+
+function init()
+{
+  canvas = $('#canvas')[0];
+  canvas.width  = (canvasHelper.blockSize * 100);
+  canvas.height = (canvasHelper.blockSize * 100);
+  context = canvas.getContext('2d');
+
+  startTime = new Date();
+
+  bikes.push(Object.create(bike));
+
+  gameLoop();
+}
+
+var startTime;
+var step = 1;
+var updateInterval = 50;
+
+var bikes = [];
+
+function gameLoop()
+{
+  var currentTime    = new Date() - startTime;
+  var nextUpdateTime = step * updateInterval;
+
+  if(currentTime > nextUpdateTime)
+  {
+    step += 1;
+    $.each(bikes, function(index, bike)
+    {
+      bike.move();
+    });
+  }
+
+  $.each(bikes, function(index, bike)
+  {
+    canvasHelper.drawBike(bike);
+  });
+
+  window.requestAnimationFrame(gameLoop);
+}
+
+$(document).ready(function(){
+  init();
 });
