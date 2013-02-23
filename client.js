c1dada88180b0b146ba7646308d5d86a0a2692ff
Alvis Duong <alvisduong@gmail.com>
2013-02-23 20:24:19 +0000

    Move socket establishment into function

--- client.js
+++ client.js
@@ -7,14 +7,19 @@
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-var socket = io.connect('/');
-socket.on('onconnected', function(data) {
-  console.log('Received UUID of ' + data.id + ' from socket.io');
-});
+function establishConnection() {
+  var socket = io.connect('/');
 
-socket.on('gameState', function(state) {
-  canvasHelper.processIncomingState(state);
-});
+  socket.on('onconnected', function(data) {
+    console.log('Received UUID of ' + data.id + ' from socket.io');
+
+    initLocalPlayer(data.id);
+  });
+
+  socket.on('gameState', function(state) {
+    canvasHelper.processIncomingState(state);
+  });
+}
 
 var canvasHelper = {
   blockSize: 5,
