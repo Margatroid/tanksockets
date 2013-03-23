ab272ccb9da28f42bb5b08568232dc4c6b483a59
Alvis Duong <alvisduong@gmail.com>
2013-03-23 16:44:36 +0000

    Bring project back to blank slate

--- client.js
+++ client.js
@@ -7,162 +7,16 @@
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
-var client = {};
-
 var socket = io.connect('/');
 
 function establishConnection() {
   socket.on('onconnected', function(data) {
     console.log('Received UUID of ' + data.id + ' from socket.io');
-
-    var localPlayer = Object.create(core.player);
-    localPlayer.init(data.id);
-    addInputHandlersToPlayer(localPlayer);
-
-    client.player = localPlayer;
-  });
-
-  socket.on('bikesBeforeStart', function(state) {
-    client.resetStartingPositions(state);
-  });
-
-  socket.on('gameState', function(state) {
-    canvasHelper.processIncomingState(state);
-  });
-
-  socket.on('startGame', function(data) {
-    client.init();
   });
 }
 
-
-var canvasHelper = {
-  blockSize: 5,
-  maxTiles: { height: 180, width: 350 },
-  stateBuffer: []
-};
-
-
-// TODO Add interpolation somehow.
-canvasHelper.processIncomingState = function(state) {
-  var ahead = core.step - state.step;
-
-  this.stateBuffer[state.step] = state;
-
-  this.setClientPositionsByState(state);
-};
-
-
-canvasHelper.checkBuffer = function() {
-  var lastState = this.stateBuffer.pop();
-
-  if(lastState.step > core.step) {
-    core.moveAllBikes();
-  }
-};
-
-
-canvasHelper.setClientPositionsByState = function(state) {
-  state.players.forEach(function(bike) {
-    var bikeToUpdate = core.getBikeById(bike.userId);
-
-    bikeToUpdate.x = bike.x;
-    bikeToUpdate.y = bike.y;
-    bikeToUpdate.direction = bike.direction;
-  });
-};
-
-
-canvasHelper.init = function() {
-  canvas = $('#canvas')[0];
-  canvas.width  = (this.blockSize * this.maxTiles.width);
-  canvas.height = (this.blockSize * this.maxTiles.height);
-  this.context  = canvas.getContext('2d');
-};
-
-
-canvasHelper.drawBike = function(bike) {
-  this.context.fillRect(
-    bike.x * this.blockSize,
-    bike.y * this.blockSize,
-    this.blockSize,
-    this.blockSize
-  );
-};
-
-
-function addInputHandlersToPlayer(player) {
-  var bike = player.bike;
-  $(document).keypress(function(event) {
-    switch(event.which) {
-      case 119: bike.changeDirection('n'); break; // W
-      case 97:  bike.changeDirection('w'); break; // A
-      case 115: bike.changeDirection('s'); break; // S
-      case 100: bike.changeDirection('e'); break; // D
-    }
-  });
-}
-
-
-core.bike.changeDirectionClient = function(newDirection) {
-  socket.emit('bikeInput', {
-    direction:  newDirection,
-    step:       core.step
-  });
-};
-
-
-core.gameLoopClient = function() {
-  $('#debug').text('X: ' + client.player.bike.x +
-      ' Y: ' + client.player.bike.y);
-
-  core.bikes.forEach(function(bike) {
-    canvasHelper.drawBike(bike);
-  });
-
-  window.requestAnimationFrame(core.gameLoop);
-};
-
-
-client.init = function() {
-  socket.emit('startServerLoop', {});
-  core.reset();
-  core.gameLoop();
-};
-
-
-client.resetStartingPositions = function(state) {
-  canvasHelper.init();
-
-  console.log('Resetting starting positions');
-  console.log(state);
-  this.addOtherPlayers(state.players);
-};
-
-
-client.addOtherPlayers = function(bikes) {
-  bikes.forEach(function(bike) {
-    if(bike.userId != client.player.id) {
-      enemyPlayer = Object.create(core.player);
-      enemyPlayer.init(bike.userId);
-    }
-  });
-};
-
-
-client.removeOtherPlayers = function() {
-  core.bikes.forEach(function(bike) {
-    if(bike.userId != client.player.id) {
-      core.removeBikeById(bike.userId);
-    }
-  });
-}
-
+var client = {};
 
 $(document).ready(function() {
   establishConnection();
-
-  $('#start_game').click(function() {
-    socket.emit('startGame', {});
-  });
 });
--- server.js
+++ server.js
@@ -21,13 +21,9 @@ sio.sockets.on('connection', function(client) {
   client.userId = uuid();
 
   client.emit('onconnected', { id: client.userId } );
-
   console.log(Date() + ' ' + client.userId + ' connected.');
 
-  onClientConnect(client);
-
   client.on('disconnect', function() {
-    onClientDisconnect(client);
     console.log(Date() + ' ' + client.userId + ' disconnected.');
   });
 });
@@ -44,88 +40,4 @@ app.get('/core.js', function (req, res) {
 app.get('/client.js', function (req, res) {
   res.sendfile(__dirname + '/client.js');
 });
-
-var hasStarted = false;
-
-function onClientConnect(newClient) {
-  newClient.player = Object.create(core.player);
-  newClient.player.init(newClient.userId);
-  newClient.player.socket = newClient;
-
-  newClient.on('bikeInput', function(client) {
-    console.log('Client: ' + client.step + ' Server: ' + core.step);
-    console.log('Client is ' + (core.step - client.step) + ' steps behind');
-    console.log('Start time is ' + core.startTime);
-    newClient.player.bike.changeDirection(client.direction);
-  });
-
-  serverCore.announcePlayersBeforeGame();
-
-  newClient.on('startGame', function(client) {
-    hasStarted = false;
-    serverCore.tellClientsToStartLoop(client.userId);
-  });
-
-
-  newClient.on('startServerLoop', function() {
-    if(!hasStarted) {
-      hasStarted = true;
-      startLoop();
-    }
-  });
-}
-
-
-function startLoop() {
-  core.reset();
-  core.gameLoop();
-}
-
-
-serverCore = {};
-
-
-serverCore.tellClientsToStartLoop = function(fromUserId) {
-  var data = { initiatingPlayerId: fromUserId };
-
-  core.bikes.forEach(function(bike) {
-    bike.player.socket.emit('startGame', data);
-  });
-};
-
-
-serverCore.updateClientsGameLoop = function() {
-  core.bikes.forEach(function(bike) {
-    bike.player.socket.emit('gameState', serverCore.gatherGameState());
-  });
-};
-
-
-serverCore.gatherGameState = function() {
-  var players = [];
-
-  core.bikes.forEach(function(bike) {
-    players.push({
-      x:          bike.x,
-      y:          bike.y,
-      direction:  bike.direction,
-      userId:     bike.player.id
-    });
-  });
-
-  return { step: core.step, players: players };
-};
-
-
-serverCore.announcePlayersBeforeGame = function() {
-  core.bikes.forEach(function(bike) {
-    bike.player.socket.emit('bikesBeforeStart', serverCore.gatherGameState());
-  });
-};
-
-
-function onClientDisconnect(client) {
-  core.removeBikeById(client.userId);
-  core.endCurrentGame();
-}
 
