9936a1f1b5175974fdffb2fd43cb60f481a8b7a9
Alvis Duong <alvisduong@gmail.com>
2013-03-24 11:31:28 +0000

    Add ability to take damage

--- core.js
+++ core.js
@@ -9,10 +9,14 @@
 function Player(uuid) {
   // Private. Player UUID.
   var _uuid = uuid;
+  var _hp   = 1;
 
   // Publicly accessible player attributes.
   this.x    = 0;
   this.y    = 0;
   this.name = _uuid;
+
+  // Privileged method to damage this player.
+  this.damage = function(damage) { _hp -= damage };
 }
 
