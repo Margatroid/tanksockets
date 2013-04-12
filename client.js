0c3e790a73a060ca5ef106a40e6f503e96c1d350
Alvis Duong <alvis@daft.ie>
2013-04-12 10:38:45 +0100

    Remove unused code

--- client.js
+++ client.js
@@ -92,58 +92,12 @@ Connection.prototype.connect = function() {
 
 function Graphics() {
   this.canvas;
-  /*
-  var canvas = new fabric.Canvas('canvas', { backgroundColor: '#EDE3BB' });
-
-  var hull = new fabric.Rect({
-    fill: 'red',
-    width: 20,
-    height: 30
-  });
-
-  var mainGun = new fabric.Rect({
-    top:    -10,
-    width:  5,
-    height: 20,
-    fill:   'black'
-  });
-
-  // Invisble gun to make gun rotate on the correct pivot.
-  var mainGunCounterBalance = new fabric.Rect({
-    fill:    'blue',
-    height:  20,
-    top:     10,
-    opacity: 0
-  });
-
-  var gun = new fabric.Group([ mainGun, mainGunCounterBalance ], {});
-
-  var tank = new fabric.Group([ hull, gun ], {
-    left: 600,
-    top:  300
-  });
-
-  canvas.add(tank);
-
-  canvas.on('mouse:move', function(options) {
-    var pointer = canvas.getPointer(options.e);
-    var angle = (Math.atan2(pointer.y - tank.top, pointer.x - tank.left)
-      * 180 / Math.PI) + 90;
-    gun.rotate(angle);
-    canvas.renderAll();
-    $('#debug').html(pointer.x + ', ' + pointer.y + '. Angle: ' + angle);
-  });
-  */
 };
 
 
 Graphics.prototype.init = function() {
   this.canvas = new fabric.Canvas('canvas', { backgroundColor: '#EDE3BB' });
 };
-
-
-
-
 
 var connection;
 var proto       = Object.getPrototypeOf;
