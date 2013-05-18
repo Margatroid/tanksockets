1963a4fd7075fdc48632ce3314d307390203a12d
Alvis Duong <alvisduong@gmail.com>
2013-05-18 21:01:38 +0100

    Remove crap code

--- client.js
+++ client.js
@@ -1,12 +1,3 @@
-// ClientWorld object.
-
-function ClientWorld() {
-  proto(ClientWorld.prototype).constructor.call(this);
-}
-
-ClientWorld.prototype   = Object.create(World.prototype);
-ClientWorld.constructor = ClientWorld;
-
 // ClientTank object.
 
 function ClientTank(player) {
@@ -65,7 +56,7 @@ ClientTank.prototype.setupTurretRotation = function() {
 // Connection object.
 
 function Connection() {
-  this.socket;
+  this.socket = {};
 }
 
 Connection.prototype.connect = function() {
@@ -91,49 +82,7 @@ Connection.prototype.connect = function() {
 // Graphics object.
 
 function Graphics() {
-  this.canvas;
-  /*
-  var canvas = new fabric.Canvas('canvas', { backgroundColor: '#EDE3BB' });
-
-  var hull = new fabric.Rect({
-    fill: 'red',
-    width: 20,
-    height: 30
-  });
-
-  var mainGun = new fabric.Rect({
-    top:    -10,
-    width:  5,
-    height: 20,
-    fill:   'black'
-  });
-
-  // Invisble gun to make gun rotate on the correct pivot.
-  var mainGunCounterBalance = new fabric.Rect({
-    fill:    'blue',
-    height:  20,
-    top:     10,
-    opacity: 0
-  });
-
-  var gun = new fabric.Group([ mainGun, mainGunCounterBalance ], {});
-
-  var tank = new fabric.Group([ hull, gun ], {
-    left: 600,
-    top:  300
-  });
-
-  canvas.add(tank);
-
-  canvas.on('mouse:move', function(options) {
-    var pointer = canvas.getPointer(options.e);
-    var angle = (Math.atan2(pointer.y - tank.top, pointer.x - tank.left)
-      * 180 / Math.PI) + 90;
-    gun.rotate(angle);
-    canvas.renderAll();
-    $('#debug').html(pointer.x + ', ' + pointer.y + '. Angle: ' + angle);
-  });
-  */
+  this.canvas = {};
 };
 
 
@@ -141,13 +90,8 @@ Graphics.prototype.init = function() {
   this.canvas = new fabric.Canvas('canvas', { backgroundColor: '#EDE3BB' });
 };
 
-
-
-
-
 var connection;
 var proto       = Object.getPrototypeOf;
-var clientWorld = new ClientWorld();
 var graphics    = new Graphics();
 
 $(document).ready(function() {
